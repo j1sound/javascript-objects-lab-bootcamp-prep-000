89e37de744e44c7b8795d42c19951b83fe331d62
JOSEPH WELCH <joewelchdrums@gmail.com>
2018-07-03 18:59:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
 }
 
 function deleteFromObjectByKey (object, key) {
-  delete object.key
-  return object
+  var newObj = Object.assign({}, obj)
+  delete newObj.key
+  return newObj
 }
