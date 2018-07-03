81fefab9a10a905a75af48c0a2a488d5f6b68873
JOSEPH WELCH <joewelchdrums@gmail.com>
2018-07-03 19:06:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes = {}
 
 function updateObjectWithKeyAndValue (object, key, value) {
   Object.assign({}, object, {key:[value]})
-  return object
+  return object[key]
 }
 
 function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
